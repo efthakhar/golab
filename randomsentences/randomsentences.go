5628b9870b919a2a9742d445f7be0b08d6a48d34
Efthakhar Bin Alam Dihab <efthakharbin@gmail.com>
2025-05-31 22:38:55 +0600

    golab wip

--- randomsentences/randomsentences.go
+++ randomsentences/randomsentences.go
@@ -9,7 +9,6 @@ var sentences = []string{
 	"Go is fast and efficient.",
 	"Always check errors.",
 	"Use goroutines wisely.",
-	"Keep your code Complex.",
 	"Keep your code simple.",
 }
 
@@ -19,5 +18,8 @@ func init() {
 
 // Random returns a random sentence from the list.
 func Random() string {
+	return sentences[rand.Intn(len(sentences))]
+}
+func Random2() string {
 	return sentences[rand.Intn(len(sentences))]
 }
